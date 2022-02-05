d11d4f1046e80c4def3705e4b283b56d71e85052
Akshit Choudhary <akshitchoudhary007@gmail.com>
2022-02-05 19:03:56 +0530

    added comments

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -45,17 +45,18 @@ contract DutchAuction {
         return startingPrice - discount;
     }
 
+    // before anyone can buy the auction, the auctioner must approve is NFT to this contract
     function buy() external payable {
         require(block.timestamp < expiresAt, "Auction has ended");
 
         uint price = getPrice();
         require(msg.value >= price, "Insuficient funds");
-
+        
         nft.transferFrom(seller, msg.sender, nftId);
         uint refund = msg.value - price;
         if (refund > 0) {
-            payable(msg.sender).transfer(refund);
+            payable(msg.sender).transfer(refund); // refund the difference
         }
-        selfdestruct(seller);
+        selfdestruct(seller); // destroy the contract to prevent further calls
     }
 }
